b608c405e32ed057c446a25c8135efd7530c3931
Jo <jo@gmail.com>
2026-07-10 16:01:43 +0300

    Second commit

--- First.cpp
+++ First.cpp
@@ -12,22 +12,8 @@ int main(){
 	cout<<"Input the 2nd number: ";
 	cin>>num2;
 	
-	if(num1 > num2){
-		
-		cout<<num1<<" is the greatest";
-		
-	}
+	double sum = num1 + num2;
 	
-	else if(num1 == num2){
-		
-		cout<<"They are equal";
-		
-	}
-	
-	else{
-		
-		cout<<num2<<" is the greatest";
-		
-	}
+	cout<<num1<< " + " <<num2<<" = "<<sum;
 	
 }
