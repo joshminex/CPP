55cf5a547702d03392cb1ff37546ff5afd130ae5
Jo <jo@gmail.com>
2026-07-10 17:38:55 +0300

    Third commit

---
+++ Sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int num = 10;
+	int sum = 0;
+	
+	for(int i=1 ; i <= num; i++){
+		
+	
+		sum += i;
+		
+		
+	}
+	
+	cout<<sum;
+	
+	
+}
